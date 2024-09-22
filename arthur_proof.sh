cefc04f4ba9a27eeecbde62dc39c2ea64af753f5
Harry Wright <harrywright@bluelightcard.co.uk>
2024-09-22 16:11:22 +0100

    refactor: put constants in caps

--- arthur_proof.sh
+++ arthur_proof.sh
@@ -1,32 +1,32 @@
 #!/bin/bash
 
-none=1
-mission_control=2
-application_windows=3
-desktop=4
-screen_saver=5
-disable_screen_saver=6
-display_sleep=10
-launchpad=11
-notification_centre=12
-lock_screen=13
-quick_note=14
+NONE=1
+MISSION_CONTROL=2
+APPLICATION_WINDOWS=3
+DESKTOP=4
+SCREEN_SAVER=5
+DISABLE_SCREEN_SAVER=6
+DISPLAY_SLEEP=10
+LAUNCHPAD=11
+NOTIFICATION_CENTRE=12
+LOCK_SCREEN=13
+QUICK_NOTE=14
 
 toggle_hot_corners() {
     # Read current hot corner settings
     top_left=$(defaults read com.apple.dock wvous-tl-corner)
 
-    if [ "$top_left" -eq "$none" ]; then
+    if [ "$top_left" -eq "$NONE" ]; then
         # Set hot corners to mission control and application windows
-        defaults write com.apple.dock wvous-tl-corner -int $mission_control
-        defaults write com.apple.dock wvous-bl-corner -int $application_windows
+        defaults write com.apple.dock wvous-tl-corner -int $MISSION_CONTROL
+        defaults write com.apple.dock wvous-bl-corner -int $APPLICATION_WINDOWS
         echo "Hot corners set to Mission Control (top left) and Application Windows (bottom left)."
     else
         # Set all corners to none
-        defaults write com.apple.dock wvous-tl-corner -int $none
-        defaults write com.apple.dock wvous-bl-corner -int $none
-        defaults write com.apple.dock wvous-tr-corner -int $none
-        defaults write com.apple.dock wvous-br-corner -int $none
+        defaults write com.apple.dock wvous-tl-corner -int $NONE
+        defaults write com.apple.dock wvous-bl-corner -int $NONE
+        defaults write com.apple.dock wvous-tr-corner -int $NONE
+        defaults write com.apple.dock wvous-br-corner -int $NONE
         echo "All hot corners turned off."
     fi
 }
